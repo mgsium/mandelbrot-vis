2e8008835c7f3afc4142b2a5e9b7de7140725ce6
Musab Guma'a <musabgumaa@gmail.com>
2020-11-28 17:42:34 +0000

    Update Mnd.java

--- mandelbrot-java/Mnd.java
+++ mandelbrot-java/Mnd.java
@@ -8,35 +8,6 @@ class Mnd
 {
     public static void main(String[] args)
     {
-		/*
-		int x = 3840;
-		int y = 2160;
-
-		long sTime = System.nanoTime();
-		boolean[] pixels = new boolean[x * y];
-
-		try {
-			FileOutputStream f = new FileOutputStream(".\\mnd.txt");
-			DataOutputStream ds = new DataOutputStream(f);
-			
-			for(int i = 0; i < x; i++) {
-				for(int j = 0; j < y; j++) {
-					Complex c = new Complex(-2 + ((double) i/ (double) x) * 3, -1 + ((double) j/ (double) y) * 2);
-					// String s = String.format("%d, %d, %d\n", i, j, mandelbrot(c, Math.sqrt(2)));
-					ds.writeBoolean(mandelbrot(c, Math.sqrt(2)) < 80 ? true : false);
-				}
-			}
-
-			ds.flush();
-			ds.close();
-			f.close();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
-		System.out.println(String.format("Execution Time : %d", (System.nanoTime() - sTime)/1000000000));
-		*/
-
 		ForkJoinPool fjp = new ForkJoinPool();
 
 		int x = 30720;
@@ -45,7 +16,6 @@ class Mnd
 		long sTime = System.nanoTime();
 
 		Mandelbrot m = new Mandelbrot(0, x, 0, y, x, y);
-		// for (boolean val : m.invoke()) System.out.print(val);
 		/*
 		try { 
 			FileOutputStream f = new FileOutputStream(".\\mnd.txt");
